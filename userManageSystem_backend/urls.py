d1ba96b53f58a45246d478431096ceb670e68232
TANGHONGBAO <10385819+tanghongbao@user.noreply.gitee.com>
2025-11-25 22:04:45 +0800

    更改项目名称为backend

--- userManageSystem_backend/urls.py
+++ userManageSystem_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for userManageSystem project.
+URL configuration for userManageSystem_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
